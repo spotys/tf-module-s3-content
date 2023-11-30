54634f6fe88924ddf6724f1304780cde78774d00
Stepan <spotys@gmail.com>
2023-11-30 18:00:39 +0100

    support for custom content-type definitions by file suffix

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
+locals {
+  suffix_map = {
+    ".vcf" : "text/vcard"
+  }
+}
+
 resource "aws_s3_object" "object" {
   for_each = fileset(var.directory, "**")
 
-  bucket = var.s3-bucket-id
-  key    = "${var.key-prefix}/${each.value}"
-  source = "${var.directory}/${each.value}"
-  etag   = filemd5("${var.directory}/${each.value}")
+  bucket       = var.s3-bucket-id
+  key          = "${var.key-prefix}/${each.value}"
+  source       = "${var.directory}/${each.value}"
+  etag         = filemd5("${var.directory}/${each.value}")
+  content_type = try([for ext, content_type in local.suffix_map : content_type if endswith(lower(each.value), ext)][0], null)
 }
