5f1104254cbc38fb3d1b2002285b2bc54bde7dbc
Stepan <spotys@gmail.com>
2023-12-15 13:59:40 +0100

    more content-type definitions added

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 locals {
   suffix_map = {
     ".vcf" : "text/vcard"
+    ".htm" : "text/html"
+    ".html" : "text/html"
+    ".css" : "text/css"
+    ".ttf" : "font/ttf"
+    ".woff2" : "font/woff2"
+    ".svg" : "image/svg+xml"
   }
 }
 
